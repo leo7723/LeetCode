53ce6d87330ae91ee1276d738f1b44e27d5a05e5
leo <leo917196100>
2022-06-16 23:32:45 +0800

    两数之和

--- src/JuneSixteen.java
+++ src/JuneSixteen.java
@@ -1,5 +1,7 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class JuneSixteen {
     public static void main(String[] args) {
@@ -21,5 +23,16 @@ public class JuneSixteen {
 
         }
         return Arrays.copyOf(res, idx + 1);
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hashMap.containsKey(nums[i])) {
+                return new int[]{hashMap.get(nums[i]), i};
+            }
+            hashMap.put(target - nums[i], i);
+        }
+        return new int[2];
     }
 }
