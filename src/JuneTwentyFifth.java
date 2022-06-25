cf3dee53825b476faab166927ce229dbc97b0ad1
leo <leo917196100>
2022-06-26 00:55:43 +0800

    盛最多水的容器

--- src/JuneTwentyFifth.java
+++ src/JuneTwentyFifth.java
@@ -15,4 +15,18 @@ public class JuneTwentyFifth {
         return res;
     }
 
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length-1;
+        int max = 0;
+        while (i < j) {
+            int temp = (j-i) * Math.min(height[i],height[j]);
+            max = Math.max(temp, max);
+            if (height[i] <= height[j]) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return max;
+    }
 }
