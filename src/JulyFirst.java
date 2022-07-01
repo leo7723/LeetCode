af59c350725461d51cc48c9288045614e77d77bc
leo <leo917196100>
2022-07-02 00:05:52 +0800

    合并K个升序链表

--- src/JulyFirst.java
+++ src/JulyFirst.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class JulyFirst {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        part("(", 1, 0, n , result);
+        part("(", 1, 0, n, result);
         return result;
     }
 
@@ -14,7 +16,49 @@ public class JulyFirst {
             result.add(str);
             return;
         }
-        part(str+'(', l+1, r, n, result);
-        part(str+')', l, r+1, n, result);
+        part(str + '(', l + 1, r, n, result);
+        part(str + ')', l, r + 1, n, result);
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode head = new ListNode();
+        PriorityQueue<ListNode> queue = new PriorityQueue<ListNode>(new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+                return o1.val - o2.val;
+            }
+        });
+
+        for (ListNode  node: lists) {
+            if (node != null) {
+                queue.add(node);
+            }
+        }
+        ListNode temp = head;
+        while (!queue.isEmpty()) {
+            temp.next = queue.poll();
+            if (temp.next.next != null) {
+                queue.add(temp.next.next);
+            }
+            temp = temp.next;
+        }
+        return head.next;
     }
 }
