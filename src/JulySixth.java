fba686f07980447d24a94c3be4e911bf043f6d95
leo <leo917196100>
2022-07-07 23:23:23 +0800

    搜索旋转排序数组

--- src/JulySixth.java
+++ src/JulySixth.java
@@ -20,4 +20,46 @@ public class JulySixth {
         }
         return maxans;
     }
+
+    /**
+     * i+j/2 = n
+
+     * nums[n] < nmus[j] 落在右边 i n 无序 n j 递增
+     * target > nums[n] < nums[j] 取 n+1 j  不然i n-1
+     * nums[n] > nmus[j] 落在左边 i n 递增 n j 无序 target > nums[i] < nums[n]
+     * target > nums[i] < nums[n] 取 i n-1  不然 n+1 j
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search(int[] nums, int target) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        if (nums.length == 1 && nums[0] == target) {
+            return 0;
+        }
+
+        int i = 0 , j = nums.length - 1;
+        while (i <= j) {
+            int n = (i+j)/2;
+            if (nums[n] == target) return n;
+            if (nums[n] < nums[nums.length-1]) {
+                if (target > nums[n] && target <= nums[j]) {
+                    i = n+1;
+                } else {
+                    j = n-1;
+                }
+            } else {
+                if (target >= nums[i] && target < nums[n]) {
+                    j = n- 1;
+                } else {
+                    i = n+1;
+                }
+            }
+        }
+        return -1;
+    }
 }
