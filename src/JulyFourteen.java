a3206ad61da31d0747066c8592f1e9164c2aba70
leo <leo917196100>
2022-07-15 00:29:23 +0800

    跳跃游戏

--- src/JulyFourteen.java
+++ src/JulyFourteen.java
@@ -11,4 +11,16 @@ public class JulyFourteen {
         }
         return result;
     }
+
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (max < i) {
+                return false;
+            }
+            max = Math.max(max, i+nums[i]);
+        }
+
+        return max >= nums.length-1;
+    }
 }
